708259054b8153f1e120fb691dd109b6b3fe3c71
Nyppp <63279872+Nyppp@users.noreply.github.com>
2025-05-26 21:32:47 +0900

    [Bronze II] Title: 커트라인, Time: 280 ms, Memory: 21184 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(arr[i] < arr[j]){
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        System.out.println(arr[N-k]);
+
+
+        sc.close();
+    }
+}

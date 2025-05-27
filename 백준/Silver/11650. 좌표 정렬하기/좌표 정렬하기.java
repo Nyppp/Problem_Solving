3fe3f4ca169c0643edfb767a26478ede7f9d2953
Nyppp <63279872+Nyppp@users.noreply.github.com>
2025-05-27 15:17:22 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 776 ms, Memory: 65740 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            String[] s = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(s[0]);
+            arr[i][1] = Integer.parseInt(s[1]);
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]){
+                    return o1[1] - o2[1];
+                }else{
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}

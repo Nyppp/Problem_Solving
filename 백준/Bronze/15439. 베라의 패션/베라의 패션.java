19a7918bbadd6890f94bc9e83090bb570a926ce0
Nyppp <63279872+Nyppp@users.noreply.github.com>
2025-06-23 17:29:00 +0900

    [Bronze IV] Title: 베라의 패션, Time: 108 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        sb.append(N * (N-1));
+        
+        System.out.println(sb);
+    }
+}

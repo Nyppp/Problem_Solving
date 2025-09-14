fb70ede305e99fd6567af237c9f6edbd72d1ae19
Nyppp <63279872+Nyppp@users.noreply.github.com>
2025-09-14 20:53:11 +0900

    [Silver III] Title: N과 M (3), Time: 540 ms, Memory: 207072 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[] arr = new int[10];
+    static boolean[] visit = new boolean[10];
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+
+        printSeq(N, M, 1);
+
+
+        System.out.println(sb);
+    }
+
+    static void printSeq(int N, int M, int depth) {
+        if(depth > M){
+            for (int i = 1; i <=M ; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+
+            arr[depth] = i;
+
+            printSeq(N,M,depth+1);
+        }
+
+    }
+}
